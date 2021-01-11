3c606777b918917778e7c60c842704eb3597d28f
Yevhenii Moloduk <72128188+Mol0D337@users.noreply.github.com>
2021-01-11 15:11:24 +0200

    Add files via upload

---
+++ precache-manifest.23b75cd6b5aa799e8ee4cfd8399d6bcb.js
@@ -0,0 +1,226 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c17c0e87624283b217b",
+    "url": "/css/app.974e3d67.css"
+  },
+  {
+    "revision": "4f95a8bdd12c623379bd",
+    "url": "/css/chunk-036ce277.7680c17b.css"
+  },
+  {
+    "revision": "8091314a5c36345e5465",
+    "url": "/css/chunk-0a54a2bc.f49697a3.css"
+  },
+  {
+    "revision": "a4645604d158c3a828fb",
+    "url": "/css/chunk-5e84110c.e2a46576.css"
+  },
+  {
+    "revision": "ec510261f030a88b0e7b",
+    "url": "/css/chunk-65dca96b.b3e2524a.css"
+  },
+  {
+    "revision": "6bc9aa21befe6e9098f2",
+    "url": "/css/chunk-890081e0.e2c8f7e0.css"
+  },
+  {
+    "revision": "4ef6e00375601ea15643",
+    "url": "/css/chunk-8a586c42.44fbba19.css"
+  },
+  {
+    "revision": "d2cb95aad043030b6186",
+    "url": "/css/chunk-96fc0afc.6d8eea4e.css"
+  },
+  {
+    "revision": "62d71533ec28d1e3900b",
+    "url": "/css/chunk-efac9d40.c2e321a8.css"
+  },
+  {
+    "revision": "f914480d1f07a8eb248f",
+    "url": "/css/chunk-vendors.73e1f5b0.css"
+  },
+  {
+    "revision": "d34c271fec82da4f865bc084d525f1a3",
+    "url": "/fonts/NotoSansRegular.d34c271f.woff2"
+  },
+  {
+    "revision": "4bff57f45c870f5b6d677d4cbcd1f60a",
+    "url": "/fonts/NotoSansSemiBold.4bff57f4.woff2"
+  },
+  {
+    "revision": "bc8ce517ef945b72f0ab1f4eaec1558e",
+    "url": "/img/191882146_images_17171766102.bc8ce517.jpg"
+  },
+  {
+    "revision": "aab2a54720396373f2562256d4761d4d",
+    "url": "/img/191882146_images_17171767068.aab2a547.jpg"
+  },
+  {
+    "revision": "40dddf3484b1d4bbb427411c3597fb76",
+    "url": "/img/191882167_images_17171766242.40dddf34.jpg"
+  },
+  {
+    "revision": "4ec3145936fc3215b3d81fac09b7e3f5",
+    "url": "/img/191882167_images_17171767733.4ec31459.jpg"
+  },
+  {
+    "revision": "bbf6f6d917cf98c499aeb531582f57fe",
+    "url": "/img/191882174_images_17171768517.bbf6f6d9.jpg"
+  },
+  {
+    "revision": "ade3bc5070f3c17d599739b849486669",
+    "url": "/img/apple_macbook_air_2020_256_space_gray_images_17747545453.ade3bc50.jpg"
+  },
+  {
+    "revision": "5fa5e5e1cfc76ea0b9a51c3fbc75059d",
+    "url": "/img/apple_macbook_air_2020_256_space_gray_images_17747546119.5fa5e5e1.jpg"
+  },
+  {
+    "revision": "c80ab6655450b30d4490d21dc9519287",
+    "url": "/img/apple_macbook_air_2020_256_space_gray_images_17747548123.c80ab665.jpg"
+  },
+  {
+    "revision": "c947dcd9d5b89c8434d4b642b32f3802",
+    "url": "/img/apple_macbook_pro_13__m1_1.c947dcd9.jpg"
+  },
+  {
+    "revision": "d1eceac755fd64f1a529a99f844e51d6",
+    "url": "/img/apple_macbook_pro_13__m1_256gb_2020_space_gray_images_20678895481.d1eceac7.jpg"
+  },
+  {
+    "revision": "9b172c9ea2557403144a81eb0fc01dbf",
+    "url": "/img/apple_macbook_pro_13__m1_256gb_2020_space_gray_images_20678895611.9b172c9e.jpg"
+  },
+  {
+    "revision": "26fbfea843cfd0ecea44227a9abae846",
+    "url": "/img/apple_muhp2_images_17178612585.26fbfea8.jpg"
+  },
+  {
+    "revision": "151ab7884d674b1eb92537fd1b687881",
+    "url": "/img/apple_muhp2_images_17178612984.151ab788.jpg"
+  },
+  {
+    "revision": "6ef4d0b7dddacd670853c1577acc9cbc",
+    "url": "/img/apple_mwp42ua_a_images_18103004533.6ef4d0b7.jpg"
+  },
+  {
+    "revision": "bc8d71fe3c9128c979abc5823dfc4bcd",
+    "url": "/img/apple_mwp42ua_a_images_181030045333.bc8d71fe.jpg"
+  },
+  {
+    "revision": "bfc751f0a38d19d4ae6f48b4f5cae2f0",
+    "url": "/img/apple_mwp42ua_a_images_18103011733333.bfc751f0.jpg"
+  },
+  {
+    "revision": "bc8d71fe3c9128c979abc5823dfc4bcd",
+    "url": "/img/apple_mxk32_a_images_18103270801.bc8d71fe.jpg"
+  },
+  {
+    "revision": "92f76c2f84d0109c56f06d6d679ba9dc",
+    "url": "/img/arrow-down.92f76c2f.svg"
+  },
+  {
+    "revision": "d498ff690a820b6f8b9a79a81178e05f",
+    "url": "/img/back.d498ff69.svg"
+  },
+  {
+    "revision": "6ef4d0b7dddacd670853c1577acc9cbc",
+    "url": "/img/copy_apple_mwp42ua_a_5eb1a14de2e3e_images_18103535749.6ef4d0b7.jpg"
+  },
+  {
+    "revision": "bc8d71fe3c9128c979abc5823dfc4bcd",
+    "url": "/img/copy_apple_mwp42ua_a_5eb1a14de2e3e_images_18103535827.bc8d71fe.jpg"
+  },
+  {
+    "revision": "bfc751f0a38d19d4ae6f48b4f5cae2f0",
+    "url": "/img/copy_apple_mwp42ua_a_5eb1a14de2e3e_images_18103535905.bfc751f0.jpg"
+  },
+  {
+    "revision": "a9bd7c5fbfe8ba3fd6e24f55e83651da",
+    "url": "/img/macbook-2.a9bd7c5f.jpg"
+  },
+  {
+    "revision": "0be7b16e03911b79d0320655e37a3848",
+    "url": "/img/macbook-3.0be7b16e.jpg"
+  },
+  {
+    "revision": "c0de7863064c20b1f81519c7a2c2b7a0",
+    "url": "/img/macbook.c0de7863.jpg"
+  },
+  {
+    "revision": "a30fbc11fe1dbb4a7cb3b3bf0dfb0837",
+    "url": "/img/mehmet-turgut-kirkgoz-vnayhPykN5Q-unsplash.a30fbc11.jpg"
+  },
+  {
+    "revision": "f5055e3b871aafd652e4074228e1d711",
+    "url": "/img/natural-paper-texture.f5055e3b.jpg"
+  },
+  {
+    "revision": "3f4e5910f60902b04ee9ca45fd909adb",
+    "url": "/img/send.3f4e5910.svg"
+  },
+  {
+    "revision": "e2cbe433921d57ba74d023084561489a",
+    "url": "/img/star.e2cbe433.svg"
+  },
+  {
+    "revision": "61b3e9c06d8d4595b29bb16a8b674858",
+    "url": "/img/trash.61b3e9c0.svg"
+  },
+  {
+    "revision": "b0fa40079d2f412fe44c8df3425b1de2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c17c0e87624283b217b",
+    "url": "/js/app.d92a7479.js"
+  },
+  {
+    "revision": "4f95a8bdd12c623379bd",
+    "url": "/js/chunk-036ce277.49b8faa5.js"
+  },
+  {
+    "revision": "8091314a5c36345e5465",
+    "url": "/js/chunk-0a54a2bc.5f885c55.js"
+  },
+  {
+    "revision": "a4645604d158c3a828fb",
+    "url": "/js/chunk-5e84110c.cd36952e.js"
+  },
+  {
+    "revision": "ec510261f030a88b0e7b",
+    "url": "/js/chunk-65dca96b.a0ec9317.js"
+  },
+  {
+    "revision": "6bc9aa21befe6e9098f2",
+    "url": "/js/chunk-890081e0.bc143c21.js"
+  },
+  {
+    "revision": "4ef6e00375601ea15643",
+    "url": "/js/chunk-8a586c42.4ea2556b.js"
+  },
+  {
+    "revision": "d2cb95aad043030b6186",
+    "url": "/js/chunk-96fc0afc.db06b713.js"
+  },
+  {
+    "revision": "30abddb85a28b37ddd73",
+    "url": "/js/chunk-d11e7c9e.a29f5d62.js"
+  },
+  {
+    "revision": "62d71533ec28d1e3900b",
+    "url": "/js/chunk-efac9d40.1e5765c2.js"
+  },
+  {
+    "revision": "f914480d1f07a8eb248f",
+    "url": "/js/chunk-vendors.a85bc4e1.js"
+  },
+  {
+    "revision": "8d4192f82ff22aae2f49028b915656fa",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
